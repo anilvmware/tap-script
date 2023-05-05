632dc2347f663b61350d13fe05eb4c676c70c7dd
anilvmware <70898535+anilvmware@users.noreply.github.com>
2023-05-05 10:59:44 -0400

    Update image-copy.sh

--- image-copy.sh
+++ image-copy.sh
@@ -5,8 +5,8 @@ docker login you-container-repo-url -u you-container-repo-username -p you-contai
 export INSTALL_REGISTRY_USERNAME=you-container-repo-username
 export INSTALL_REGISTRY_PASSWORD=you-container-repo-passwrod
 export INSTALL_REGISTRY_HOSTNAME=you-container-repo-url
-export TAP_VERSION=1.4.2
-export INSTALL_REPO=tap14
+export TAP_VERSION=1.5.0
+export INSTALL_REPO=tap15
 imgpkg copy -b registry.tanzu.vmware.com/tanzu-application-platform/tap-packages:${TAP_VERSION} --to-repo ${INSTALL_REGISTRY_HOSTNAME}/${INSTALL_REPO}/tap-packages
 
 imgpkg copy -b registry.tanzu.vmware.com/tanzu-application-platform/full-tbs-deps-package-repo:1.7.4 \
